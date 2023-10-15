4fe0cf10037718f9a4e7d88f4b0b3f70295505a7
tiaditaa <118504168+tiaditaa@users.noreply.github.com>
2023-10-15 16:39:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,20 @@ Route::get('/tambahdatapribadi', [PenggunaController::class, 'tambahdatapribadi'
 Route::get('/tambahberkaspendukung', [PenggunaController::class, 'tambahberkaspendukung'])->name('tambahberkaspendukung');
 
 Route::post('/insertdata', [PenggunaController::class, 'insertdata'])->name('insertdata');
+
+//mutia
+Route::get('/datapekerjaan', [FormControllers::class, 'index'])->name('datapekerjaan');
+
+Route::get('/tambahpekerjaan', [FormControllers::class, 'tambahpekerjaan'])->name('tambahpekerjaan');
+
+Route::post('/insertpekerjaan', [FormControllers::class, 'insertpekerjaan'])->name('insertpekerjaan');
+
+Route::get('/tampilkanpekerjaan/{id}', [FormControllers::class, 'tampilkanpekerjaan'])->name('tampilkanpekerjaan');
+
+Route::post('/updatepekerjaan/{id}', [FormControllers::class, 'updatepekerjaan'])->name('updatepekerjaan');
+
+Route::get('/deletepekerjaan/{id}', [FormControllers::class, 'deletepekerjaan'])->name('deletepekerjaan');
+
+Route::get('/output', function () {
+    return view('output');
+});
