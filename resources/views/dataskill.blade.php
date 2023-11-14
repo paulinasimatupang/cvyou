3c60fe27dc41e7125ed6abc1b63dadc4a87d1d45
Jonanda27 <123913405+Jonanda27@users.noreply.github.com>
2023-11-14 17:07:40 +0700

    update dataskill
    kiw

--- resources/views/dataskill.blade.php
+++ resources/views/dataskill.blade.php
@@ -1,6 +1,35 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <style>
+        .rating {
+            display: flex;
+            align-items: center;
+        }
+
+        .rating input {
+            display: none;
+        }
+
+        .rating label {
+            color: #ddd;
+            margin: 0;
+        }
+
+        .rating label:before {
+            content: '\2605';
+            font-size: 25px;
+            padding: 5px;
+            padding-bottom: 0;
+            cursor: pointer;
+        }
+
+        .rating input:checked ~ label {
+            color: #FFD700;
+        }
+    </style>
+
+
     <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,7 +37,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <title>CVYou | Data Skill </title>
+  <title>CVYou | Data Skill</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="{{ asset('path-to-bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -22,28 +51,39 @@
         {{ session('success') }}
     </div>
   @endif
-    
+
     <h1 class="text-center mb-4">Data Skill</h1>
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-8">
           <div class="card">
             <div class="card-body">
-              <form action="/insertdataskill" method="POST" enctype="multipart/form-data">
+              <form action="/insertdatapekerjaan" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-3">
-                  <label for="exampleInputskill" class="form-label">Skill</label>
-                  <input type="text" name="skill" class="form-control" id="exampleInputskill" aria-describedby="emailHelp">
+                  <label for="examplePengalaman" class="form-label">Skill</label>
+                  <input type="text" name="skill" class="form-control" id="exampleInputPengalaman" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
-                  <label for="exampleInputrating" class="form-label">Rating</label>
-                  <input type="text" name="rating" class="form-control" id="exampleInputrating" aria-describedby="emailHelp">
-                </div>
+                    <label for="exampleInputRating" class="form-label">Rating</label>
+                    <div class="rating">
+                        <input type="radio" name="rating" value="1" id="1">
+                        <label for="1" id="star1"></label>
+                        <input type="radio" name="rating" value="2" id="2">
+                        <label for="2"></label>
+                        <input type="radio" name="rating" value="3" id="3">
+                        <label for="3"></label>
+                        <input type="radio" name="rating" value="4" id="4">
+                        <label for="4"></label>
+                        <input type="radio" name="rating" value="5" id="5">
+                        <label for="5"></label>
+
+                    </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
               <div>
-                <a href="/tambahdatapendidikan" class="btn btn-success">Back</a>
+                <a href="/tambahdataskill" class="btn btn-success">Back</a>
               </div>
               <div>
                 <a href="/tambahberkaspendukung" class="btn btn-success">Next</a>
@@ -90,7 +130,7 @@
               <td>{{ $row->rating }}</td>
               <td>
                 <a href="/editdataskill/{{ $row->id }}" type="button" class="btn btn-warning">Edit</a>
-                <a href="/deleteskill/{{ $row->id }}" type="button" class="btn btn-danger delete" data-id="{{ $row->id }}">Delete</a>
+                <a href="#" type="button" class="btn btn-danger delete" data-id="{{ $row->id }}">Delete</a>
               </td>
             </tr>
             @endforeach
@@ -126,11 +166,11 @@
           swal("Data tidak jadi dihapus!");
         }
       });
-    }); 
+    });
   </script>
-  {{-- <script>
-    @if (Session::has('success'))
-    toastr.success("{{ Session::get('success') }}")
-    @endif
-  </script> --}}
+  <script>
+    // @if (Session::has('success'))
+    // toastr.success("{{ Session::get('success') }}")
+    // @endif
+  </script>
 </html>
